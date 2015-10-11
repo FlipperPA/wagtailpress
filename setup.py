ce28f2bd7065c072504db10e4daed9917b519c10
Allen, Timothy <tallen@wharton.upenn.edu>
2015-10-11 15:39:10 +0000

    Switch to RST.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -9,11 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='wagtailpress',
     version='0.1',
-    packages=['md_cms'],
+    packages=['wagtailpress'],
     include_package_data=True,
     license='BSD License',
     description='wagtailpress is an Django app which extends the Wagtail CMS to be similar to WordPress.',
-    long_description=README,
     long_description=open('README.rst', encoding='utf-8').read(),
     url='https://github.com/FlipperPA/wagtailpress',
     author='Timothy Allen',
