0cb89ef2f1091b269f867e3682a23ad58b3ebab1
Allen, Timothy <tallen@wharton.upenn.edu>
2016-02-28 15:14:23 +0000

    Tweaks / renames to fit WordPress naming conventions.

--- wagtailpress/templatetags/wagtailpress_tags.py
+++ wagtailpress/templatetags/wagtailpress_tags.py
@@ -1,22 +1,30 @@
+# from datetime import date
+from django import template
+# from django.conf import settings
+
+# from demo.models import PersonPage, BlogPage, EventPage, Advert, Page
+
+register = template.Library()
+
 @register.assignment_tag(takes_context=True)
 def get_site_root(context):
-	"""
-	Gets the root page for the site. Returns a wagtailcore.Page model.
-	"""
+    """
+    Gets the root page for the site. Returns a wagtailcore.Page model.
+    """
 
     return context['request'].site.root_page
 
 
 def has_menu_children(page):
-	"""
-	Returns a boolean for whether or not the current page has child pages.
-	"""
+    """
+    Returns a boolean for whether or not the current page has child pages.
+    """
 
     return page.get_children().live().in_menu().exists()
 
 
-@register.inclusion_tag('wagtailpress/tags/nav.html', takes_context=True)
-def top_menu(context, parent, calling_page=None):
+@register.inclusion_tag('wagtailpress/tags/nav_menu.html', takes_context=True)
+def nav_menu(context, parent, calling_page=None):
     """
     Retrieves the top menu items - the immediate children of the parent page
     The has_menu_children method is necessary because the bootstrap menu requires
@@ -34,6 +42,19 @@ def top_menu(context, parent, calling_page=None):
     return {
         'calling_page': calling_page,
         'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+# Retrieves the children of the top menu items for the drop downs
+@register.inclusion_tag('wagtailpress/tags/nav_menu_children.html', takes_context=True)
+def nav_menu_children(context, parent):
+    menuitems_children = parent.get_children()
+    menuitems_children = menuitems_children.live().in_menu()
+    return {
+        'parent': parent,
+        'menuitems_children': menuitems_children,
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
     }
